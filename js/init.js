0d9ac63c2055827e406796367322c31694703a7e
José Cabarcas <josecabarcas94@gmail.com>
2015-03-02 12:28:12 -0500

    Commit para la primera page

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+  	$('.button-collapse').sideNav({menuWidth: 240, activationWidth: 70});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
